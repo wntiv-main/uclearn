9040452ab1935a9a56dbaa72a5d642c722de8106
Callum Hynes <chy54@uclive.ac.nz>
2025-02-24 13:44:45 +1300

    improved mathlive/stack interop, mathlive debug styles

--- src/global/constants.ts
+++ src/global/constants.ts
@@ -11,3 +11,4 @@ export const DEBUG = !!+(process.env.UCLEARN_DEBUG ?? 0);
 const DEBUG_FLAGS = DEBUG ? (process.env.UCLEARN_DEBUG_FLAGS ?? "").toLowerCase().split(/,\s*/g).filter(x => x) : [];
 export const DEBUG_HYDRATION = DEBUG_FLAGS.includes('hydration');
 export const DEBUG_SCRIPTING = DEBUG_FLAGS.includes('scripting');
+export const DEBUG_MATHLIVE = DEBUG_FLAGS.includes('mathlive');
--- src/learn/mathlive-loader.ts
+++ src/learn/mathlive-loader.ts
@@ -1,7 +1,10 @@
 import { MathfieldElement } from "mathlive";
+import type { Expression } from "@cortex-js/compute-engine";
+import "@cortex-js/compute-engine";
 import { AMTparseAMtoTeX } from "asciimath";
 
 import { EXT_URL } from "./constants";
+import { DEBUG_MATHLIVE } from "../global/constants";
 
 MathfieldElement.soundsDirectory = null;
 MathfieldElement.fontsDirectory = `${EXT_URL}/learn/mathlive/`;
@@ -9,6 +12,42 @@ MathfieldElement.fontsDirectory = `${EXT_URL}/learn/mathlive/`;
 export const MATHLIVE_FIELD_CLASS = '__uclearn-mathlive-field';
 
 export const MATH_FIELD_SELECTOR = '.que.stack input:is(.algebraic, .numerical)';
+
+if(DEBUG_MATHLIVE) {
+	document.documentElement.classList.add('__uclearn-mathlive-debug');
+}
+
+function mathJSONtoStack(mathJson: Expression): string {
+	if(typeof mathJson === 'string' || typeof mathJson === 'number') return `${mathJson}`;
+	if(!Array.isArray(mathJson)) return '<TODO>'/* TODO */;
+	const [op, ...args] = mathJson;
+	switch(op) {
+		case 'Error': {
+			if(args[0] === "'missing'") return '';
+			return `<${args}>`;
+		}
+		case 'Add': {
+			const [a, b] = args;
+			return `(${mathJSONtoStack(a)} + ${mathJSONtoStack(b)})`;
+		}
+		case 'Power': {
+			const [a, b] = args;
+			return `(${mathJSONtoStack(a)})^(${mathJSONtoStack(b)})`;
+		}
+		case 'Divide': {
+			const [a, b] = args;
+			return `(${mathJSONtoStack(a)})/(${mathJSONtoStack(b)})`;
+		}
+		case 'Multiply':
+			return args.map(mathJSONtoStack).join('*');
+		case 'Ln':
+			return `ln(${mathJSONtoStack(args[0])})`;
+		case 'Sequence':
+			return args.map(mathJSONtoStack).join(' ');
+		default:
+			return '<TODO>';
+	}
+}
 
 export function initField(field: HTMLInputElement & ChildNode) {
 	const mathField = new MathfieldElement({
@@ -26,16 +65,23 @@ export function initField(field: HTMLInputElement & ChildNode) {
 			bottom: calc(0.5em - 0.5lh);
 		}
 	`;
+	mathField.addEventListener('mount', () => {
+		mathField.menuItems = mathField.menuItems.filter(item => !/color|variant|decoration/.test((item as { id?: string; }).id ?? ''));
+	});
 	mathField.shadowRoot?.append(styleOverrides);
 	mathField.readOnly = field.readOnly;
-	mathField.menuItems = mathField.menuItems.filter(item => !(item as { id?: string; }).id?.includes('color'));
 	mathField.removeExtraneousParentheses = true;
 	mathField.smartFence = true;
 	mathField.smartSuperscript = true;
 
 	mathField.setValue(AMTparseAMtoTeX(field.value));
 	mathField.classList.add(MATHLIVE_FIELD_CLASS);
-	mathField.addEventListener('input', () => { field.value = mathField.getValue('ascii-math'); });
+	mathField.addEventListener('input', e => {
+		const mathJson = JSON.parse(mathField.getValue('math-json'));
+		console.log(mathJson);
+		field.value = mathJSONtoStack(mathJson);
+		field.dispatchEvent(new InputEvent('input', e));
+	});
 	mathField.addEventListener('change', e => { field.dispatchEvent(new InputEvent('change', e)); });
 	field.before(mathField);
 }
